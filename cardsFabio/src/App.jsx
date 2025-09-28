acaaea9384007678405fbfd9c81f0b8e08f12f96
felipeFleite <felipefracalossileite@gmail.com>
2025-09-28 18:08:15 -0400

    update main.css/ added responsivity, prices and descriptions

--- cardsFabio/src/App.jsx
+++ cardsFabio/src/App.jsx
@@ -1,5 +1,5 @@
 import "./main.css"
-
+import React from "react";
 function App() {
 
   return (
@@ -13,96 +13,104 @@ function App() {
       <img src="../public/1.png" alt="1" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Camisa Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Estampa inspirada em controles e um design moderno em neon</h3>
       </div>
+      <div className="price">R$ 99,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/2.png" alt="2" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Pá Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Um acessório inusitado com estilo gamer e acabamento em neon.</h3>
       </div>
+      <div className="price">R$ 59,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/3.png" alt="3" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Ventilador Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Potência e estilo com iluminação neon para refrescar suas partidas.</h3>
       </div>
+      <div className="price">R$ 149,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/4.png" alt="4" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Carro Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Miniatura colecionável com visual futurista e detalhes em neon gamer.</h3>
       </div>
+      <div className="price">R$ 199,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/5.png" alt="5" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Garrafa Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Design ergonômico com estampa gamer em neon, perfeita para hidratar durante as jogatinas.</h3>
       </div>
+      <div className="price">R$ 39,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/6.png" alt="6" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Churrasqueira Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Churrasco com estilo: iluminação neon e tema gamer para os verdadeiros mestres da grelha.</h3>
       </div>
+      <div className="price">R$ 299,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/7.png" alt="7" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Violão Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Som autêntico com design moderno e detalhes em neon, unindo música e games.</h3>
       </div>
+      <div className="price">R$ 249,90</div>
     </div>
     <div className="cardOverall">
       <div className="img">
       <img src="../public/8.png" alt="8" />
       </div>
       <div className="name">
-      <h2>chupa leite 1</h2>
+      <h2>Liquidificador Gamer</h2>
       </div>
 
       <div className="desc">
-        <h3>chupa leite 2</h3>
+        <h3>Prepare suas poções energéticas com estilo neon gamer e tecnologia moderna.</h3>
       </div>
+      <div className="price">R$ 119,90</div>
     </div>
   </div>
     </>
